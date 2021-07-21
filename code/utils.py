8fb05ab851eb1e151451103d8c5006807c07a697
HibernationNo1 <winter4958@gmail.com>
2021-07-21 16:17:44 +0900

    (find_max_confidence_bounding_box) prediction Bbox when confidence more than 0.5

--- code/utils.py
+++ code/utils.py
@@ -111,9 +111,13 @@ def find_max_confidence_bounding_box(bounding_box_info_list):
 	bounding_box_info_list_sorted = sorted(bounding_box_info_list,
 											key=itemgetter('confidence'),
 											reverse=True)
-	max_confidence_bounding_box = bounding_box_info_list_sorted[0]
+	confidence_bounding_box_list = list()
 
-	return max_confidence_bounding_box
+	for i in range(2):  # 0부터 1까지 상위 두 개의 confidence값을 가진 bounding_box
+		if bounding_box_info_list_sorted[i]['confidence'] > 0.5:  # 상위 bounding_box값이 0.5 이상일 때만 Bbox를 표현하기 위한 조건
+			confidence_bounding_box_list.append(bounding_box_info_list_sorted[i])
+
+	return confidence_bounding_box_list
 
 
 def yolo_format_to_bounding_box_dict(xcenter, ycenter, box_w, box_h, class_name, confidence):
@@ -135,32 +139,39 @@ def iou(yolo_pred_boxes, ground_truth_boxes):
 	boxes1 = yolo_pred_boxes
 	boxes2 = ground_truth_boxes
 
+	# yolo_pred_boxes의 중앙 좌표  0:x, 1:y, 2:w, 3:y
 	boxes1 = tf.stack([boxes1[:, :, :, 0] - boxes1[:, :, :, 2] / 2, boxes1[:, :, :, 1] - boxes1[:, :, :, 3] / 2,
 					   boxes1[:, :, :, 0] + boxes1[:, :, :, 2] / 2, boxes1[:, :, :, 1] + boxes1[:, :, :, 3] / 2])
-	boxes1 = tf.transpose(boxes1, [1, 2, 3, 0])
+	boxes1 = tf.transpose(boxes1, [1, 2, 3, 0]) 	# shape을 [4 7 7 2] 에서 [7 7 2 4]로 변경
+
+	# ground_truth_boxes의 중앙 좌표
 	boxes2 = tf.stack([boxes2[0] - boxes2[2] / 2, boxes2[1] - boxes2[3] / 2,
 					   boxes2[0] + boxes2[2] / 2, boxes2[1] + boxes2[3] / 2])
 	boxes2 = tf.cast(boxes2, tf.float32)
+	# tf.shape(boxes1) == [4]
 
 	# calculate the left up point
-	lu = tf.maximum(boxes1[:, :, :, 0:2], boxes2[0:2])
-	rd = tf.minimum(boxes1[:, :, :, 2:], boxes2[2:])
+	lu = tf.maximum(boxes1[:, :, :, 0:2], boxes2[0:2])  # 두 Bbox 중 x, y의 최대값(전체 영역의 우측 상단 꼭지점 좌표)
+	rd = tf.minimum(boxes1[:, :, :, 2:], boxes2[2:])	# 두 Bbox 중 w, h의 최소값(교집합 영역의 우측 상단 꼭지점 좌표)
   
 	# intersection
-	intersection = rd - lu
+	intersection = rd - lu  
 
-	inter_square = intersection[:, :, :, 0] * intersection[:, :, :, 1]
+	inter_square = intersection[:, :, :, 0] * intersection[:, :, :, 1] # 모든 x좌표 * y좌표
 
+	# 위에서 계산된 intersection 영역 중에서 0인 영역만이 진짜 교집합 영역
 	mask = tf.cast(intersection[:, :, :, 0] > 0, tf.float32) * tf.cast(intersection[:, :, :, 1] > 0, tf.float32)
 
+	# 각 cell 마다, 그리고 각 Bbox마다 교집합 영역 계산
 	inter_square = mask * inter_square
 
 	# calculate the boxs1 square and boxs2 square
-	square1 = (boxes1[:, :, :, 2] - boxes1[:, :, :, 0]) * (boxes1[:, :, :, 3] - boxes1[:, :, :, 1])
-	square2 = (boxes2[2] - boxes2[0]) * (boxes2[3] - boxes2[1])
+	square1 = (boxes1[:, :, :, 2] - boxes1[:, :, :, 0]) * (boxes1[:, :, :, 3] - boxes1[:, :, :, 1]) # yolo_pred_boxes의 넓이
+	square2 = (boxes2[2] - boxes2[0]) * (boxes2[3] - boxes2[1]) 									# ground_truth_boxes의 넓이
 
 	iou = inter_square / (square1 + square2 - inter_square + 1e-6)
 	# '1e-6' : for the denominator to not zero
+	# 교집합 영역이 없으면 iou는 zero
 
 	return iou
 
