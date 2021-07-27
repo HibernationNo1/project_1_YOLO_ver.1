24d98a1ab80e5ac6a6ae020948915953a0b03299
HibernationNo1 <winter4958@gmail.com>
2021-07-27 18:49:56 +0900

    fix class_loss, confidence_loss

--- code/loss.py
+++ code/loss.py
@@ -10,8 +10,6 @@ from dataset import class_name_dict
 def yolo_loss(predict,
 			  labels,
 			  each_object_num,
-			  num_classes,
-			  boxes_per_cell,
 			  cell_size,
 			  input_width,
 			  input_height,
@@ -22,10 +20,12 @@ def yolo_loss(predict,
 			  class_loss_object,
 			  confidence_loss_object):
 
-	# parse only coordinate vector
-	# predict의 shape [tf.shape(predict)[0], cell_size, cell_size, num_classes + 5 * boxes_per_cell]
-	predict_boxes = predict[:, :, num_classes + boxes_per_cell:]
-	predict_boxes = tf.reshape(predict_boxes, [cell_size, cell_size, boxes_per_cell, 4])
+	# predict[0] == pred_class 		[1, cell_size, cell_size, num_class]
+	# predict[1] == pred_confidence [1, cell_size, cell_size, boxes_per_cell]
+	# predict[2] == pred_coordinate	[1, cell_size, cell_size, boxes_per_cell, 4]
+		
+	predict_boxes = predict[2]
+	predict_boxes = tf.squeeze(predict_boxes)
 
 	# prediction : absolute coordinate
 	pred_xcenter = predict_boxes[:, :, :, 0]
@@ -37,14 +37,13 @@ def yolo_loss(predict,
 
 	# parse labe
 	labels = np.array(labels) 
-	for i in range(4):	# tf.shape(labels) == [1, 1, 1, 1, [2]] 이기 때문에 for문 사용
-		labels[:, i] = labels[:, i].astype('float32')
 	label = labels[each_object_num, :]
 
 	xcenter = label[0]
-	ycenter = label[1]
+	ycenter = label[1] 
 	sqrt_w = tf.sqrt(label[2])
 	sqrt_h = tf.sqrt(label[3])
+
 
 	# calulate iou between ground-truth and predictions
 	# 각 cell의 각 Bbox와 label과의 iou계산 tf.shape(iou_predict_truth):, [7 7 2]
@@ -56,30 +55,24 @@ def yolo_loss(predict,
 	best_box_mask = tf.cast((I >= max_I), tf.float32) # IOU가 가장 큰 call == object가 위치한 cell
 
 	# set object_loss information(confidence, object가 있을 확률)
-	C = iou_predict_truth # object가 있는 cell에만 적용될 값이고, 해당 cell에서 IOU는 1이다.
-	pred_C = predict[:, :, num_classes:num_classes + boxes_per_cell]
-	temp_pred_C = np.zeros_like(pred_C)
-	for i in range(cell_size):
-			for j in range(cell_size):
-				temp_pred_C[i][j] = tf.sigmoid(pred_C[i][j]) 
-	pred_C = tf.constant(temp_pred_C)
+	C = iou_predict_truth # object가 있는 cell에만 적용될 값이고, 해당 cell에서 IOU는 1이다.	
+	pred_C = predict[1]
+	pred_C = tf.squeeze(pred_C)
 	
 	# set class_loss information(probability, 특정 class일 확률)
-	P = label[4]
-
-	pred_P = predict[:, :, 0:num_classes] 
-	temp_pred_P = np.zeros_like(pred_P)
-	for i in range(cell_size):  # 각 cell의 예측값에 대해 activation function을 적용한다.
-			for j in range(cell_size):
-				temp_pred_P[i][j] = tf.nn.softmax(pred_P[i][j]) 
-	pred_P = tf.constant(temp_pred_P)
+	temp_P = np.zeros_like(pred_C)
+	for i in range(cell_size):
+		for j in range(cell_size):
+				temp_P[i][j] = label[4]
+	P = tf.constant(temp_P)
+	pred_P = predict[0]
+	pred_P = tf.squeeze(pred_P)
 
 	# find object exists cell mask
 	object_exists_cell = np.zeros([cell_size, cell_size, 1])
 	object_exists_cell_i, object_exists_cell_j = int(cell_size * ycenter / input_height), int(cell_size * xcenter / input_width)
 	object_exists_cell[object_exists_cell_i][object_exists_cell_j] = 1
 
-	# set coord_loss
 	coord_loss = (tf.nn.l2_loss(object_exists_cell * best_box_mask * (pred_xcenter - xcenter) / (input_width / cell_size)) +
 					tf.nn.l2_loss(object_exists_cell * best_box_mask * (pred_ycenter - ycenter) / (input_height / cell_size)) +
 					tf.nn.l2_loss(object_exists_cell * best_box_mask * (pred_sqrt_w - sqrt_w)) / input_width +
