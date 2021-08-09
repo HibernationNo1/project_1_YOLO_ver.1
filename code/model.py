9c97cbbd9f67de89d6a6514ca7475be1da852280
TaeUk Noh <winter4958@gmail.com>
2021-08-09 12:57:22 +0900

    add regularization method

--- code/model.py
+++ code/model.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.regularizers import L1, L2
 from tensorflow.keras.applications import InceptionV3
 
 # Implementation using tf.keras.applications (https://www.tensorflow.org/api_docs/python/tf/keras/applications)
@@ -14,24 +15,28 @@ class YOLOv1(Model):
 		# shape = (None, 5, 5, 2048)
 		base_model.trainable = True
 		x = base_model.output
+		x = Dropout(0.2)(x)
     
 		# Global Average Pooling
 		x = GlobalAveragePooling2D()(x)  # shape = (None, 2048)
 		x = Dense(cell_size * cell_size * (num_classes + (boxes_per_cell*5)), activation=None)(x)
+		x = Dropout(0.2)(x)
 		# flatten vector -> cell_size x cell_size x (num_classes + 5 * boxes_per_cell)
-		
 		pred_class = x[:,  : cell_size * cell_size * num_classes]
+		pred_class = Dense(cell_size * cell_size * num_classes, activation=None, kernel_regularizer = L2(l = 0.02))(pred_class)
+
 		pred_confidence = x[:,  cell_size * cell_size * num_classes: (cell_size * cell_size * num_classes) + (cell_size * cell_size * boxes_per_cell)]
+		pred_confidence = Dense(cell_size * cell_size * boxes_per_cell, activation=None, kernel_regularizer = L2(l=0.01))(pred_confidence)
+
 		pred_coordinate = x[:, (cell_size * cell_size * num_classes) + (cell_size * cell_size * boxes_per_cell): ]
-		
+		pred_coordinate = Dense(cell_size * cell_size * (boxes_per_cell*4), activation=None, kernel_regularizer = L1(l=0.08))(pred_coordinate)		
+
+
 		pred_class = tf.reshape(pred_class, 
 				 [tf.shape(pred_class)[0], cell_size, cell_size, num_classes])
-		pred_class = tf.nn.softmax(pred_class) 
-		# tf.reduce_sum(pred_class) == cell_size * cell_size
 
 		pred_confidence = tf.reshape(pred_confidence, 
-				 [tf.shape(pred_confidence)[0], cell_size, cell_size, boxes_per_cell])
-		pred_confidence = tf.nn.sigmoid(pred_confidence)		
+				 [tf.shape(pred_confidence)[0], cell_size, cell_size, boxes_per_cell])		
 
 		pred_coordinate = tf.reshape(pred_coordinate,
 				 [tf.shape(pred_coordinate)[0], cell_size, cell_size, boxes_per_cell, 4])
